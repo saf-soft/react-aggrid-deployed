66b9d8a1877212bd07ac9b5385733356133c44bf
saf-soft <63599366+saf-soft@users.noreply.github.com>
2021-05-17 21:53:08 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Countrygrid from './Countrygrid';
 function App() {
   return (
     <div>
-    <center>React App</center>
+    <div align="center" style={{color:"#f7df1e",font:"9",fontWeight:"bold"}}>React App</div>
     <br></br>
     <div align="center" style={{color:"red",font:"7",fontWeight:"bold"}}>This is data is fetched from aggrid website</div>
     <br></br>
