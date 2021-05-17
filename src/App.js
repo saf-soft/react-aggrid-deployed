a632996e9635610db1c1d43003e13db27779ecec
saf-soft <63599366+saf-soft@users.noreply.github.com>
2021-05-17 22:17:32 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,14 +11,18 @@ function App() {
      <br></br>
     <div align="center" style={{color:"red",font:"7",fontWeight:"bold"}}>This is data is fetched from aggrid website</div>
     <br></br>
+    <div align="center" style={{color:"#651e3e",font:"7",fontWeight:"bold"}}>Automobile ShowRoom</div>
+    <br></br>
    <Apigrid></Apigrid>
    <br></br>
-   <div align="center" style={{color:"red",font:"7",fontWeight:"bold"}}>This is data is fetched from country rest API</div>
+   <div align="center" style={{color:"#ff6f69",font:"7",fontWeight:"bold"}}>This is data is fetched from country rest API</div>
    <br></br>
+       <div align="center" style={{color:"#ff3377",font:"7",fontWeight:"bold"}}>Country Lists</div>
+    <br></br>
    <Countrygrid></Countrygrid>
     <br></br>
     <hr></hr>
-    <div align="center" style={{color:"cornflowerblue",font:"5",fontWeight:"bold"}}>Developed by Safwan Mannambath</div>
+    <div align="center" style={{color:"#4b3832",font:"5",fontWeight:"bold"}}>Developed by Safwan Mannambath</div>
      <br></br>
      <br></br>
      <br></br>
